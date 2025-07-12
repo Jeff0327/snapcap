defbaa398de3b484456403f9ceeac008e12d9545
jeff0327 <cocacola158500@gmail.com>
2025-07-13 02:08:08 +0900

    build error fix

--- app/(main)/forget/password/page.tsx
+++ app/(main)/forget/password/page.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import FindPasswordForm from "@/app/(main)/forget/password/form";
 
 function Page() {
     return (
         <div className={'mt-0 md:mt-16'}>
-            {/*<FindPasswordForm/>*/}
         </div>
     );
 }
